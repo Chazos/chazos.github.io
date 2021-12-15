26e2ba2dff3b3ba6bf46e8cf5d1ebd8c2957abad
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-12-15 15:13:59 +0200

    Changed config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Chazos',
+  tagline: 'Another Laravel Headless CMS',
+  url: 'https://chazos.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Chazos', // Usually your GitHub org/user name.
+  projectName: 'chazos.github.io', // Usually your repo name.
+  deploymentBranch:'main',
 
   presets: [
     [
